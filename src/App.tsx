8a1af5f16db0dd5ff8e0138bb22e26d73dca7b65
AndersonCodhugo <anderson_hindo@hotmail.com>
2026-08-12 09:48:44 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="quadro">
+      <div className='primeira-linha-vertical'></div>
+      <div className='segunda-linha-vertical'></div>
+      <div className='linha-horizontal'></div>
+
+      <h1>Meu Projeto</h1>
+    </div>
+  )
+}
+
+export default App
